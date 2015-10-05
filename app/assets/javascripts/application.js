8b119efcc0ff141e77674a9a397dcc9941882f71
Bruno Pagno <brunopagno@gmail.com>
2015-10-05 14:42:52 -0300

    Added basic d3 stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require jquery_ujs
 //= require foundation
 //= require d3
-//= require_tree .
+//= require_tree ./vis
 
 $(function(){ $(document).foundation(); });
--- app/assets/javascripts/vis/clock.js
+++ app/assets/javascripts/vis/clock.js
@@ -1,7 +1,14 @@
 function clock(element, data) {
+  var rect = d3.select(element).node().getBoundingClientRect();
+
   var svg = d3.select(element).append('svg')
       .attr('width', 100)
       .attr('height', 100)
-    .append('g')
-      .attr('transform', "translate(50, 50)");
+    .append('g');
+
+  var circle = svg.append("circle")
+      .attr('fill', 'red')
+      .attr('cx', 50)
+      .attr('cy', 50)
+      .attr('r', 50);
 }
