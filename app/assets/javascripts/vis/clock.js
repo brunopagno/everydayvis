731dc42782d8aa52281e9148595a72f783e80536
Bruno Pagno <brunopagno@gmail.com>
2015-10-06 00:13:33 -0300

    Added partial clock visualization

--- app/assets/javascripts/vis/clock.js
+++ app/assets/javascripts/vis/clock.js
@@ -1,14 +1,81 @@
 function clock(element, data) {
-  var rect = d3.select(element).node().getBoundingClientRect();
+  var width = 250;
+  var height = 250;
+  var radius = Math.min(width, height) / 2;
+  var activityRadius = 0.7 * radius;
+  var luminosityRadius = 0.55 * radius;
+  var dayAndNightRadius = 0.4 * radius;
 
-  var svg = d3.select(element).append('svg')
-      .attr('width', 100)
-      .attr('height', 100)
-    .append('g');
+  var activity_scale = d3.scale.linear().domain([0, data.max_activity]).range([0, 100]);
 
-  var circle = svg.append("circle")
-      .attr('fill', 'red')
-      .attr('cx', 50)
-      .attr('cy', 50)
-      .attr('r', 50);
+  var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return 1; });
+
+  // Activity
+
+  var activityArc = d3.svg.arc()
+      .innerRadius(activityRadius)
+      .outerRadius(function(d) { 
+        return (radius - activityRadius) * (activity_scale(d.data) / 100.0) + activityRadius;
+      });
+
+  var activityOutlineArc = d3.svg.arc()
+      .innerRadius(activityRadius)
+      .outerRadius(radius);
+
+  var svg = d3.select(element).append("svg")
+      .attr("class", "daily-clock")
+      .attr("width", width)
+      .attr("height", height);
+
+  var activitySvg = svg.append("g")
+      .attr("class", "activity-arc")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  var path = activitySvg.selectAll(".solid-arc")
+      .data(pie(data.activities))
+    .enter().append("path")
+      .attr("class", "solid-arc")
+      .attr("d", activityArc);
+
+  var outerPath = activitySvg.selectAll(".outline-arc")
+      .data(pie(data.activities))
+    .enter().append("path")
+      .attr("class", "outline-arc")
+      .attr("d", activityOutlineArc);
+
+  // Luminosity
+
+  var luminositySvg = svg.append("g")
+      .attr("class", "luminosity-arc")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  var luminosityArc = d3.svg.arc()
+      .innerRadius(luminosityRadius)
+      .outerRadius(activityRadius - 2);
+
+  var luminosityOuterPath = luminositySvg.selectAll(".luminosity-outline-arc")
+      .data(pie(data.luminosity))
+    .enter().append("path")
+      .attr("class", "luminosity-outline-arc")
+      .attr("fill", function(d) { return "rgb(" + d.data * 3 + "," + d.data * 2.5 + "," + d.data * 1.5 + ")"; })
+      .attr("d", luminosityArc);
+
+  // Day and night
+
+  var dayAndNightSvg = svg.append("g")
+      .attr("class", "day-and-night-arc")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  var dayAndNightArc = d3.svg.arc()
+      .innerRadius(dayAndNightRadius)
+      .outerRadius(luminosityRadius - 2);
+
+  var dayAndNightOuterPath = dayAndNightSvg.selectAll(".luminosity-outline-arc")
+      .data(pie(data.dayAndNight))
+    .enter().append("path")
+      .attr("class", "luminosity-outline-arc")
+      .attr("fill", function(d) { return "rgb(" + d.data * 3 + "," + d.data * 2.5 + "," + d.data * 1.5 + ")"; })
+      .attr("d", dayAndNightArc);
 }
