0519978fcb26b02951ec3180c6ac209bd188f96b
Bruno Pagno <brunopagno@gmail.com>
2015-10-15 01:10:37 -0300

    Seeding data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+Dir['quidata/*.csv'].each do |file_name|
+  next if File.directory? file_name
+  puts 'reading: ' + file_name.split('/')[-1]
+  relevant_stuff = false
+  CSV.foreach(file_name) do |row|
+    if relevant_stuff
+      next unless row[0]
+      dd = row[1].split('/')
+      tt = row[2].split(':')
+
+      datetime = DateTime.new(dd[2].to_i, dd[1].to_i, dd[0].to_i,
+                              tt[0].to_i, tt[1].to_i, tt[2].to_i)
+      activity = row[3].to_i
+      light = row[5].to_f
+      awake = row[7] == 'ACTIVE'
+
+      # create model here
+    end
+
+    relevant_stuff = true if row.join(',').start_with? "Line,Date,Time,Activity,Marker,White Light,Sleep/Wake,Interval Status"
+  end
+end
+
+# Models~~
+# 
+# Person/Subject/Patient/User
+#   code:string, identity:string, name:string, gender:string, age:integer
+# 
+# Activity
+#   datetime:datetime, activity:integer, light:float, awake:boolean, person:references
