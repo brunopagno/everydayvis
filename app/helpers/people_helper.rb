c86d3a1fb63bb8dd4fac2229f9d5664f6a67d1e6
Bruno Pagno <brunopagno@gmail.com>
2015-11-03 20:08:37 -0200

    Fixed date with incorrect timezone

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,7 +3,7 @@ module PeopleHelper
   def daily_activities_to_json(user, daily)
     return {
       user_id: user.id,
-      date: daily.first[:datetime].to_date,
+      date: daily.first[:datetime],
       activities: daily.map{ |d| d[:activity] },
       luminosity: daily.map{ |d| d[:light] },
       sunrise: DateTime.new(2000, 1, 1, 6, 58, 0),
