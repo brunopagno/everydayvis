818e21a0cd8a48e4d401701fe91b3693d9db4669
Bruno Pagno <brunopagno@gmail.com>
2015-11-04 12:39:53 -0200

    Partial update

--- app/assets/javascripts/vis/calendar.js
+++ app/assets/javascripts/vis/calendar.js
@@ -27,9 +27,27 @@ function fillCalendar(element, data) {
       events: evs,
       eventRender: function(ev, el) {
         el.addClass("day-item");
-        el.css('height', '10px');
-        el.css('padding', '12px');
+        el.css('height', '5px');
+        el.css('padding', '6px 12px 14px 12px');
         el.css('font-weight', 'bold');
+
+        var checkbox = $('<input type=checkbox class="calendar-box"></input>')
+        checkbox.attr("value", ev.start);
+
+        el.append(checkbox);
+
+        // var svg = $("<svg></svg>");
+        // svg.attr("width", 100)
+        //    .attr("height", 100);
+
+        // var circle = $("<circle></circle>");
+        // circle.attr("cx", 10)
+        //       .attr("cy", 10)
+        //       .attr("r", 50)
+        //       .attr("fill", "red");
+
+        // el.append(svg);
+        // svg.append(circle);
       }
   });
 }
