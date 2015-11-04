0837cdc969e116e0870f3f396002bf7058e823b5
bocattelan <bocattelan@gmail.com>
2015-11-04 22:49:45 +0000

    Página de links para autenticação (jawbone - antigo)

---
+++ app/controllers/jawbone_controller.rb
@@ -0,0 +1,6 @@
+class JawboneController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
   
-  get 'C:\Users\Bruno\Workspace\maquinaVirtual\everydayvis\app\views\maps', to: 'maps#iframe'
+  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\maps', to: 'maps#iframe'
 
-  resources :people,:maps, only: [:index, :show]
+  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\login', to: 'login#frame'
+
+  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\login\jawbone', to: 'jawbone#frame'
+
+  resources :login,:people,:maps,:jawbone, only: [:index, :show]
 
 
 end
