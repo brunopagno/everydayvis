06b5555847d132188d29b5667484d64a210f8266
bocattelan <bocattelan@gmail.com>
2015-11-06 00:37:43 +0000

    Path Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
   
-  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\maps', to: 'maps#iframe'
+  #get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\maps', to: 'maps#iframe'
 
-  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\login', to: 'login#frame'
+#  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\login', to: 'login#frame'
 
-  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\login\jawbone', to: 'jawbone#frame'
+#  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\login\jawbone', to: 'jawbone#frame'
 
-  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\jawboneLogged', to: 'jawbonelogged#iframe'
+#  get 'C:\Users\Bruno\Workspace\maquinaVirtual\pagno\app\views\jawboneLogged', to: 'jawbonelogged#iframe'
 
   resources :login,:people,:maps,:jawbone,:jawbonelogged, only: [:index, :show]
 
