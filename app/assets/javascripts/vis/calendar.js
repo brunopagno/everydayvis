6d580ac9fb7d3fddf4b98490b5683ae90ce0dc89
Bruno Pagno <brunopagno@gmail.com>
2015-11-24 11:10:55 -0200

    Histogram of full day working fine

--- app/assets/javascripts/vis/calendar.js
+++ app/assets/javascripts/vis/calendar.js
@@ -65,7 +65,7 @@ function fillCalendar(element, data) {
           var histo = d3.select(element).append("div").attr("class", "full-histogram");
 
           histo.append("div")
-            .attr("class", "close-histo")
+            .attr("class", "close-histo float-right")
           .append("a")
             .attr("class", "button round alert tiny")
             .text("X")
@@ -82,7 +82,53 @@ function fillCalendar(element, data) {
           $.ajax({
             url: url,
             success: function(data) {
-              
+              var margin = {top: 12, right: 24, bottom: 18, left: 40};
+              var width = 1024 - margin.left - margin.right;
+              var height = 250 - margin.top - margin.bottom;
+
+              var x = d3.time.scale().range([0, width]);
+              var y = d3.scale.linear().range([height, 0]);
+              var xAxis = d3.svg.axis().scale(x).orient("bottom");
+              var yAxis = d3.svg.axis().scale(y).orient("left");
+
+              var area = d3.svg.area()
+                  .x(function(d) { return x(d.datetime); })
+                  .y0(height)
+                  .y1(function(d) { return y(d.activity); });
+
+              var svg = histo.append("div").attr("class", "graph").append("svg")
+                  .attr("width", width + margin.left + margin.right)
+                  .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+              data.forEach(function(d) {
+                d.datetime = new Date(d.datetime);
+                d.activity = +d.activity;
+              });
+
+              x.domain(d3.extent(data, function(d) { return d.datetime; }));
+              y.domain([0, d3.max(data, function(d) { return d.activity; })]);
+
+              svg.append("path")
+                  .datum(data)
+                  .attr("class", "area")
+                  .attr("d", area);
+
+              svg.append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", "translate(0," + height + ")")
+                  .call(xAxis);
+
+              svg.append("g")
+                  .attr("class", "y axis")
+                  .call(yAxis)
+                .append("text")
+                  .attr("transform", "rotate(-90)")
+                  .attr("y", 6)
+                  .attr("dy", ".71em")
+                  .style("text-anchor", "end")
+                  .text("Activity");
             },
             error: function() {
               console.log("error loading histogram data");
