71fd1f244a24fd2c9ccba0863b42465875f1bb52
Bruno Pagno <brunopagno@gmail.com>
2015-12-10 18:50:34 -0200

    Rolled back toold coloring in calendar

--- app/assets/javascripts/vis/calendar.js
+++ app/assets/javascripts/vis/calendar.js
@@ -2,10 +2,7 @@ var calendarSelectedDays = [];
 
 function fillCalendar(element, data) {
   var max_activity = d3.max(data, function(d) { return d.activity } );
-  if (max_activity < 10000) {
-    max_activity = 10000;
-  }
-  var steps_color = d3.scale.linear().domain([0, 5000, 10000, max_activity]).range(["#ff1a1a", "#ffff1a", "#99ff33", "#1aff1a"]);
+  var steps_color = d3.scale.linear().domain([0, max_activity / 2, max_activity]).range(["#ff1a1a", "#ffff1a", "#1aff1a"]);
 
   var max_work = d3.max(data, function(d) { return d.work } );
   var work_color = d3.scale.linear().domain([0, max_work / 2, max_work]).range(["#bbbbbb", "#6699ff"]);
