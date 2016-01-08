99cb65697e1dcdda2aac14f920a5687f20716c4d
bocattelan <bocattelan@gmail.com>
2016-01-08 17:30:47 +0000

    adicionando no routes o login

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   get 'person/:id/max_activity/:year/:month/:day', to: 'people#max_activity', constraints: { format: :json }
   get 'person/:id/:year/:month/:day/:hour', to: 'people#person_hour', constraints: { format: :json }
   get 'weather/:year/:month/:day/:latitude/:longitude', to: 'weather#show', constraints: { format: :json }
+  get 'weather/:year/:month/:day/:latitude/:longitude', to: 'weather#show', constraints: { format: :json }
+
+  get fitbitLogin, to 'login#index'
 
   resources :login,:people,:maps,:jawbone,:jawbonelogged, only: [:index, :show]
 end
