0f2359fc7763da319b0c254fd37707642b4bd4bb
Cattelan <Cattelan>
2016-01-14 15:42:33 -0200

    Fitbit com rails ao inves de javascript

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -4,6 +4,11 @@ class FoursquareController < ApplicationController
   end
 
   def code
+  	uri = URI("https://foursquare.com/oauth2/access_token?client_id=DXNTVHRHFKEM2RUW5RPP1WL40AWM2OWZADS53LA1BPAATHNR&client_secret=CIN4INFRTBVUVIKHENWOKLOR4VKYKMUDOGXKTXNVCIHTO0TP&grant_type=authorization_code&redirect_uri=http://nedel.cloudapp.net/foursquarecode&code=" + params[:code])
+  	request = Net::HTTP::Get.new(uri.to_s, {})
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    @response = http.request(request)
   end
 
   def logged
