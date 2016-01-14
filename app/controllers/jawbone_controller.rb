29ceccf9b7c6c4e2ddf2ec3434c43165c8139dca
Cattelan <Cattelan>
2016-01-14 11:50:21 -0200

    Pagno ajudando com o code

--- app/controllers/jawbone_controller.rb
+++ app/controllers/jawbone_controller.rb
@@ -4,6 +4,11 @@ class JawboneController < ApplicationController
   end
 
   def code
+  	uri = URI("https://jawbone.com/auth/oauth2/token?client_id=7NkRJA8m8sg&client_secret=59f2f3e8915b065113d9e54a76eda13672066004&grant_type=authorization_code&code=" + params[:code])
+  	request = Net::HTTP::Get.new(uri.to_s, {})
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    @response = http.request(request)
   end
 
   def logged
