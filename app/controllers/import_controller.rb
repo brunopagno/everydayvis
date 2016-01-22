88cbca68c215546a22a3c37cb037d775df6f7fd6
bocattelan <bruno_cattelan02@hotmail.com>
2016-01-22 16:50:26 -0200

    upload com rails

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,10 +1,9 @@
 class ImportController < ApplicationController
 
   def index
-    if 'person' in params:
-        uploaded_io = params[:person][:picture]
-        File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-        file.write(uploaded_io.read)
+    #uploaded_io = params[:person][:picture]
+    #File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+    #file.write(uploaded_io.read)
     end
   end
 end
