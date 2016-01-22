353726595f8d8f0cf8e16fe1d58b77c10b0bec30
bocattelan <bruno_cattelan02@hotmail.com>
2016-01-22 16:51:06 -0200

    upload com rails

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -4,6 +4,6 @@ class ImportController < ApplicationController
     #uploaded_io = params[:person][:picture]
     #File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
     #file.write(uploaded_io.read)
-    end
+    #end
   end
 end
