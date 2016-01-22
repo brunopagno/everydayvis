50e0282a14062b56d98802245ce48c3ac4a35b7b
bocattelan <bruno_cattelan02@hotmail.com>
2016-01-22 19:34:50 -0200

    AJAX test

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,7 +1,6 @@
 class ImportController < ApplicationController
-  @@files = 0
+  @@files ||=[]
   def index
-    @@files = 0
     #uploaded_io = params[:person][:picture]
     #File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
     #file.write(uploaded_io.read)
