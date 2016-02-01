d86f6ee149091b7d5808c107ba8c1f883baab31d
Bruno Pagno <brunopagno@gmail.com>
2016-02-01 20:02:28 -0200

    Something agani

--- app/controllers/smada_controller.rb
+++ app/controllers/smada_controller.rb
@@ -2,13 +2,13 @@ class SmadaController < ApplicationController
 
   def dados
     @dados = []
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 1)
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 2)
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 3)
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 4)
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 5)
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 6)
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 7)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 1")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 2")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 3")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 4")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 5")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 6")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 7")
   end
 
 end
