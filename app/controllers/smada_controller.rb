13d38b1ff2df93d8517408319f0c1d0e8e2f8cb0
Bruno Pagno <brunopagno@gmail.com>
2016-02-01 20:00:36 -0200

    Added something to test on azure

--- app/controllers/smada_controller.rb
+++ app/controllers/smada_controller.rb
@@ -1,7 +1,14 @@
 class SmadaController < ApplicationController
 
   def dados
-    @dados = Smada.conn.execute("SELECT * FROM dados")
+    @dados = []
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 1)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 2)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 3)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 4)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 5)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 6)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE timestamp > ? AND sub_sensor_id = ?", Date.new(2014, 1, 1), 7)
   end
 
 end
